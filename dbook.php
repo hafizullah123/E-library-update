c5459e8ef91d8f7a318d4812997685f661fd8724
Hafizullah <hjihadwal@gmail.com>
2025-05-19 10:53:58 +0430

    add the session

--- dbook.php
+++ dbook.php
@@ -1,4 +1,6 @@
 <?php
+session_start(); // Add this at the very top
+
 // Language selection (defaults to English if not set)
 $lang = 'dbooken'; // Default language
 if (isset($_GET['lang']) && in_array($_GET['lang'], ['dbooken', 'dbookps', 'dbookfa'])) {
